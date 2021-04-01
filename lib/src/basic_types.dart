d56f9a1422be7e2742b5c673fd7f51b54b8faf4e
bambinoua <bambinoua@gmail.com>
2021-04-01 10:50:18 +0300

    Add `Date` type

--- lib/src/basic_types.dart
+++ lib/src/basic_types.dart
@@ -3,3 +3,20 @@ typedef InstanceBuilder<T> = T Function();
 
 /// Signature for a function that creates an instance of type T from `map`.
 typedef InstanceMapBuilder<T> = T Function(Map<String, dynamic> map);
+
+class Date extends DateTime {
+  ///
+  Date(int year, [int month = DateTime.january, int day = 1])
+      : super(year, month, day, 0, 0, 0, 0, 0);
+
+  ///
+  Date.today() : super.now();
+
+  ///
+  Date.fromMicrosecondsSinceEpoch(int microsecondsSinceEpoch)
+      : super.fromMicrosecondsSinceEpoch(microsecondsSinceEpoch);
+
+  ///
+  Date.fromMillisecondsSinceEpoch(int millisecondsSinceEpoch)
+      : super.fromMillisecondsSinceEpoch(millisecondsSinceEpoch);
+}
