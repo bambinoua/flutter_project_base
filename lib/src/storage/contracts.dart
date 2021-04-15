b487d299f1f415a92aa38a08c645cdbd0bf86941
BambinoUA <bambinoua@gmail.com>
2021-04-15 18:57:00 +0300

    Update `Storage` contracts

--- lib/src/storage/contracts.dart
+++ lib/src/storage/contracts.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter_project_base/src/basic_types.dart';
+import 'package:flutter_project_base/src/contracts.dart';
+
+/// Specifies a priority setting that is used to decide whether
+/// to evict a storafe entry.
+enum StorageItemPriority {
+  /// Indicates that there is no priority for removing the cache entry.
+  standard,
+
+  ///Indicates that a cache entry should never be removed from the cache.
+  notRemovable
+}
 
 /// Defines list of available storage providers.
 enum StorageProvider {
@@ -15,43 +27,60 @@ enum StorageProvider {
   webSession,
 }
 
+/// A key that uses as parameter for [IStorage].
+class StorageItem<T> extends KeyValuePair<String, T>
+    implements Cloneable<StorageItem<T>> {
+  /// Construct a [StorageItem] with optional persistence.
+  const StorageItem({
+    required String key,
+    required T value,
+    this.priority = StorageItemPriority.standard,
+  }) : super(key, value);
+
+  /// Priority setting that is used to determine whether to evict
+  /// a storage entry.
+  final StorageItemPriority priority;
+
+  @override
+  StorageItem<T> copyWith({
+    T? value,
+  }) =>
+      StorageItem<T>(
+        key: key,
+        value: value ?? this.value,
+        priority: priority,
+      );
+
+  @override
+  String toString() =>
+      'StorageItem (key: $key, value: $value, priority: ${describeEnum(priority)})';
+}
+
 /// The Storage interface provides access to a particular mobile, memory,
 /// domain's session or local storage. It allows, for example, the addition,
 /// modification, or deletion of stored data items.
-abstract class Storage {
-  Storage._();
+abstract class IStorage {
+  IStorage._();
 
   /// When invoked, will empty all keys out of the storage.
   void clear();
 
-  /// Return that `key`'s value, or `null` if the key does not exist,
-  /// in the given storage.
-  T? getItem<T>(StorageKey key);
+  /// Retrieves the value of the item from the storage associated with this
+  /// object's `key`, or `null` if the key does not exist.
+  StorageItem<T> getItem<T>(String key, {T? defaultValue});
 
   /// Returns list of item keys of this storage.
-  List<StorageKey> get keys;
+  List<String> get keys;
 
   /// Returns number of data items stored in this storage.
   int get length;
 
-  /// Adds the passed `key` to the storage, or update that `key`'s
-  /// `value`  if it already exists.
-  void putItem<T>(StorageKey key, T value);
+  /// Adds the passed storage item to the storage, or update that `item`
+  /// if it already exists.
+  void putItem<T>(StorageItem<T> item);
 
-  /// Removes the `key` from the given Storage object if it exists.
+  /// Removes the `key` from the given storage object if it exists.
   /// If there is no item associated with the given key, this method
   /// will do nothing.
-  void removeItem(StorageKey key);
-}
-
-/// A key that uses as parameter for [Storage].
-class StorageKey extends ValueKey<String> {
-  /// Construct a [StorageKey] with optional persistence.
-  const StorageKey(
-    String value, {
-    this.persistent = false,
-  }) : super(value);
-
-  /// Returns `true` if key will be persistent between sessions.
-  final bool persistent;
+  void removeItem(String key);
 }
