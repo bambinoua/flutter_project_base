c09b1193e34f8b6f68bb58d790b3a1aaea920191
BambinoUA <45417992+bambinoua@users.noreply.github.com>
2022-07-15 19:03:25 +0300

    Rename `GeneralException` to `Emergency`

--- lib/src/core/exceptions.dart
+++ lib/src/core/exceptions.dart
@@ -1,15 +1,17 @@
 /// A marker interface implemented by app exceptions.
 ///
-/// The `GeneralException class is the superclass of all exceptions which will be
+/// The [Emergency] class is the superclass of all exceptions which will be
 /// created using the package. Only objects that are instances of this class
 /// (or one of its subclasses) can be thrown by the Dart `throw` statement.
-class GeneralException implements Exception {
+class Emergency implements Exception {
   /// Creates a new exception with an optional error `message`.
-  GeneralException({this.message, this.code = ''})
-      : stackTrace = StackTrace.current;
+  const Emergency({
+    required this.message,
+    this.code = '',
+  }) : assert(message.length > 0);
 
   /// The long form message of the exception.
-  final String? message;
+  final String message;
 
   /// The optional code to accommodate the message.
   ///
@@ -18,9 +20,14 @@ class GeneralException implements Exception {
 
   /// The stack trace which provides information to the user about the call
   /// sequence that triggered an exception
-  final StackTrace stackTrace;
+  StackTrace get stackTrace => StackTrace.current;
 
   @override
-  String toString() =>
-      message == null ? 'GeneralException ($code)' : '${message!} ($code)';
+  String toString() {
+    final props = <String>[
+      if (code.isNotEmpty) '($code)',
+      message,
+    ].join(', ');
+    return 'Emergency {$props}';
+  }
 }
