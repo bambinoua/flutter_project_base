6635d05452698aeca5b8e524ce7dfc26cfbccef3
BambinoUA <45417992+bambinoua@users.noreply.github.com>
2022-07-19 19:09:34 +0300

    Add HTTP client test

--- test/http/client_test.dart
+++ test/http/client_test.dart
@@ -4,12 +4,13 @@ import 'package:flutter_project_base/flutter_project_base.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('DIO client test', () async {
+  test('DIO client GET test', () async {
     try {
-      final httpClient = DioHttpClient(baseUrl: 'https://google.com');
-      final response = await httpClient.get(Uri.parse('/'));
-      print(response);
-      expect(1, 1);
+      const numberApiBaseUrl = 'http://numbersapi.com';
+      final httpClient = DioHttpClient(baseUrl: numberApiBaseUrl);
+      final response =
+          await httpClient.get(Uri.parse('/100/trivia')) as DioClientResponse;
+      expect(response.data.statusCode, 200);
     } on Emergency catch (e) {
       fail(e.message);
     }
