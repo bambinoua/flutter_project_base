3ea686f19438ce7d08447241738c01ede109d87f
BambinoUA <45417992+bambinoua@users.noreply.github.com>
2022-07-29 10:20:20 +0300

    Update `RegularExpressions`

--- lib/src/core/constants.dart
+++ lib/src/core/constants.dart
@@ -63,7 +63,13 @@ class ColorAnsiCode {
 class RegularExpressions {
   RegularExpressions._();
 
+  /// Whether string formatted as ISO-8601 datetime.
+  ///
+  /// Example: 2022-02-22T10:10:10.100Z
   static final dateIso8601 = RegExp(
       r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{1,6})?Z?$',
       caseSensitive: false);
+
+  /// Whether string is a numeric.
+  static final numeric = RegExp(r'^-?\d*\.?\d*$');
 }
