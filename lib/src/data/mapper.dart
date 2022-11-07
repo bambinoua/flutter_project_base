2e2eaf5017031633c9605435eae6cb1fcf069771
BambinoUA <45417992+bambinoua@users.noreply.github.com>
2022-11-07 14:01:54 +0200

    Update `EntityMapper`

--- lib/src/data/mapper.dart
+++ lib/src/data/mapper.dart
@@ -1,3 +1,4 @@
+import '../core/basic_types.dart';
 import '../core/domain_driven_design.dart';
 
 /// Declares an interface for bidirectional mapping of [Entity] to [DTO]
@@ -9,4 +10,13 @@ abstract class EntityMapper<T extends Entity, S extends DTO>
 
   /// Maps the `dto` to [Entity].
   T toEntity(S dto);
+
+  /// Maps the `map` to [DTO].
+  S toDTOFromMap(JsonMap map);
+
+  List<S> toListOfDTOs(List<T> entities) =>
+      entities.map((e) => toDTO(e)).toList();
+
+  List<T> toListOfEntities(List<S> dtos) =>
+      dtos.map((o) => toEntity(o)).toList();
 }
