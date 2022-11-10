c3ae735a99c7bdaa646a59b5be3c79541755c688
BambinoUA <45417992+bambinoua@users.noreply.github.com>
2022-11-10 15:38:47 +0200

    Export packages

--- lib/flutter_project_base.dart
+++ lib/flutter_project_base.dart
@@ -2,6 +2,8 @@ library flutter_project_base;
 
 export 'package:collection/collection.dart';
 export 'package:equatable/equatable.dart';
+export 'package:device_info_plus/device_info_plus.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
 export 'package:freezed_annotation/freezed_annotation.dart';
 export 'package:json_annotation/json_annotation.dart';
 export 'package:google_fonts/google_fonts.dart';
